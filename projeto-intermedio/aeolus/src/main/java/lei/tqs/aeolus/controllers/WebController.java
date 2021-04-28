a74634b4da3fad063ecb2fc4980a1ffa487bea9a
Orlando-pt <orlando.macedo15@ua.pt>
2021-04-28 18:26:29 +0100

    steal logger

--- projeto-intermedio/aeolus/src/main/java/lei/tqs/aeolus/controllers/WebController.java
+++ projeto-intermedio/aeolus/src/main/java/lei/tqs/aeolus/controllers/WebController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@Log4j2
 public class WebController {
 
     @GetMapping("/")
--- projeto-intermedio/aeolus/src/main/java/lei/tqs/aeolus/utils/TimeConverter.java
+++ projeto-intermedio/aeolus/src/main/java/lei/tqs/aeolus/utils/TimeConverter.java
@@ -4,6 +4,10 @@ import java.util.Calendar;
 
 public class TimeConverter {
 
+    private TimeConverter() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static Calendar dateFromTimeUnix(long timeUnix) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(timeUnix * 1000);
